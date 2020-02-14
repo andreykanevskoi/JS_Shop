6e402a36c2477ee6185c5fbbada59b2c88b2c173
kanandedu <kanandedu@gmail.com>
2020-02-14 18:20:26 +0300

    Show my first React-MDL object :)

--- src/client/App.js
+++ src/client/App.js
@@ -3,6 +3,10 @@ import './app.css';
 import { DataTable, TableHeader } from 'react-mdl';
 // import ReactImage from "./react.png";
 
+import 'react-mdl/extra/material';
+import 'react-mdl/extra/material.css';
+// import 'react-mdl/css/prism,css';
+
 export default class App extends Component {
   state = {
     id: -1,
@@ -25,16 +29,16 @@ export default class App extends Component {
   render() {
     const userObj = this.state;
     return (
-      <div>
-        <DataTable shadow={0} rows={[{ id: userObj.id, email: userObj.email }]}>
-          <TableHeader numeric name="id" tooltip="Unique user's number">
-            USER_ID
-          </TableHeader>
-          <TableHeader name="email" tooltip="User's email">
-            USER_EMAIL
-          </TableHeader>
-        </DataTable>
-      </div>
+    // <div>
+      <DataTable shadow={0} rows={[{ id: userObj.id, email: userObj.email }]}>
+        <TableHeader numeric name="id" tooltip="Unique user's number">
+          USER_ID
+        </TableHeader>
+        <TableHeader name="email" tooltip="User's email">
+          USER_EMAIL
+        </TableHeader>
+      </DataTable>
+    // </div>
     );
   }
 }
